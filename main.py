27e402e3b4fe5f7e965a566445630fe481044c1b
Marco112358 <marcomarco112358@gmail.com>
2023-01-31 20:40:38 -0500

    Refactored main

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ user_ip_usdt = 3000.0
 user_ip_dai = 1500.0
 user_pwripor = 1000.0
 
+user_ip_tkns = [user_ip_usdc, user_ip_usdt, user_ip_dai]
+
 ## Global Parameters ##
 vrt_shft = 1.0
 base_boost = 0.4
@@ -26,29 +28,21 @@ deleg_amts = []
 for i, tkn in enumerate(tkns):
     stkd_amts.append(float(pools_df.loc[tkn, 'stakedIpTokenAmount']))
     deleg_amts.append(float(pools_df.loc[tkn, 'delegatedPwIporAmount']))
-ipusdc_stk = stkd_amts[0]
-pwripor_stk_usdc = deleg_amts[0]
-ipusdt_stk = stkd_amts[1]
-pwripor_stk_usdt = deleg_amts[1]
-ipdai_stk = stkd_amts[2]
-pwripor_stk_dai = deleg_amts[2]
 ipor_prc = fn.get_price('ipor', 'usd')
 
 
 def objective(x, sign=-1.0):
-    user_pwripor_usdc = user_pwripor * x[0]
-    user_pwripor_usdt = user_pwripor * x[1]
-    user_pwripor_dai = user_pwripor * x[2]
-    new_pwripor_stk_usdc = pwripor_stk_usdc + user_pwripor_usdc
-    new_pwripor_stk_usdt = pwripor_stk_usdt + user_pwripor_usdt
-    new_pwripor_stk_dai = pwripor_stk_dai + user_pwripor_dai
-    user_usdc_apr = fn.staking_pool(ipusdc_stk, new_pwripor_stk_usdc, ipor_prc, vrt_shft, base_boost, log_base,
-                                    horz_shft, tkn_per_block, blocks_per_yr, user_ip_usdc, user_pwripor_usdc)
-    user_usdt_apr = fn.staking_pool(ipusdt_stk, new_pwripor_stk_usdt, ipor_prc, vrt_shft, base_boost, log_base,
-                                    horz_shft, tkn_per_block, blocks_per_yr, user_ip_usdt, user_pwripor_usdt)
-    user_dai_apr = fn.staking_pool(ipdai_stk, new_pwripor_stk_dai, ipor_prc, vrt_shft, base_boost, log_base, horz_shft,
-                                   tkn_per_block, blocks_per_yr, user_ip_dai, user_pwripor_dai)
-    return sign * np.sum([user_usdc_apr, user_usdt_apr, user_dai_apr])
+    user_pwripor_list = np.multiply(x, user_pwripor)
+    new_pwripor_list = deleg_amts + user_pwripor_list
+    user_apr = []
+    for t in np.arange(0, len(tkns)):
+        if user_ip_tkns[t] == 0:
+            apr = 0.0
+        else:
+            apr = fn.staking_pool(stkd_amts[t], new_pwripor_list[t], ipor_prc, vrt_shft, base_boost, log_base,
+                                  horz_shft, tkn_per_block, blocks_per_yr, user_ip_tkns[t], user_pwripor_list[t])
+        user_apr.append(apr)
+    return sign * np.sum([user_apr])
 
 
 def constraint1(x):
@@ -68,20 +62,19 @@ sol = minimize(objective, x0, method='SLSQP', bounds=bnds, constraints=cons)
 max_delegation_strat = sol.x
 
 ## Running final output ##
+user_pwripor_list = np.multiply(max_delegation_strat, user_pwripor)
+new_pwripor_list = deleg_amts + user_pwripor_list
+user_apr_list = []
+for t in np.arange(0, len(tkns)):
+    apr = fn.staking_pool(stkd_amts[t], new_pwripor_list[t], ipor_prc, vrt_shft, base_boost, log_base,
+                          horz_shft, tkn_per_block, blocks_per_yr, user_ip_tkns[t], user_pwripor_list[t])
+    user_apr_list.append(apr)
+
+## Format the Tables for output
 user_delegation = pd.DataFrame(index=['pct'], columns=['USDC', 'USDT', 'DAI'])
 user_delegation.iloc[0, :] = max_delegation_strat
-user_usdc_apr = fn.staking_pool(ipusdc_stk, pwripor_stk_usdc, ipor_prc, vrt_shft, base_boost, log_base, horz_shft,
-                                tkn_per_block, blocks_per_yr, user_ip_usdc,
-                                user_pwripor * user_delegation.loc[:, 'USDC'][0])
-user_usdt_apr = fn.staking_pool(ipusdt_stk, pwripor_stk_usdt, ipor_prc, vrt_shft, base_boost, log_base, horz_shft,
-                                tkn_per_block, blocks_per_yr, user_ip_usdt,
-                                user_pwripor * user_delegation.loc[:, 'USDT'][0])
-user_dai_apr = fn.staking_pool(ipdai_stk, pwripor_stk_dai, ipor_prc, vrt_shft, base_boost, log_base, horz_shft,
-                               tkn_per_block, blocks_per_yr, user_ip_dai,
-                               user_pwripor * user_delegation.loc[:, 'DAI'][0])
-
 user_aprs = pd.DataFrame(index=['pct'], columns=['USDC', 'USDT', 'DAI'])
-user_aprs.iloc[0, :] = [user_usdc_apr, user_usdt_apr, user_dai_apr]
+user_aprs.iloc[0, :] = user_apr_list
 user_tot_aprs = np.sum(user_aprs, axis=1)[0]
 
 print('You should allocate your pwrIPOR tokens using this delegation strategy:')
