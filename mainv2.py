d667acca5d7ca5aa2857626a35d4cada67e63ef4
Marco112358 <marcomarco112358@gmail.com>
2023-01-31 23:05:23 -0500

    added costs for purchase

--- mainv2.py
+++ mainv2.py
@@ -13,8 +13,8 @@ ipdai_prc = 1.0406
 ## USER Parameters ##
 user_ipusdc = 2000.0
 user_ipusdt = 0.0
-user_ipdai = 0.0
-user_pwripor = 1000.0
+user_ipdai = 500.0
+user_pwripor = 100.0
 change = 100
 
 ip_prcs = [ipusdc_prc, ipusdt_prc, ipdai_prc]  # Will need to update if add new tokens
@@ -124,20 +124,23 @@ user_iptkns_df = pd.DataFrame(index=['ipTkn Amounts'], columns=['USDC', 'USDT', 
 user_iptkns_df.iloc[0, :] = new_ip_tkns
 user_iptkns_chng = pd.DataFrame(index=['ipTkn Amounts'], columns=['USDC', 'USDT', 'DAI'])
 user_iptkns_chng.iloc[0, :] = np.subtract(new_ip_tkns, user_ip_tkns)
-cost = np.sum(np.multiply(np.subtract(new_ip_tkns, user_ip_tkns), ip_prcs)) \
-       + np.multiply(np.subtract(new_pw_tkn, user_pwripor), ipor_prc)
+iptokn_cost = np.sum(np.multiply(np.subtract(new_ip_tkns, user_ip_tkns), ip_prcs))
+ipor_cost = np.multiply(np.subtract(new_pw_tkn, user_pwripor), ipor_prc)
+total_cost = iptokn_cost + ipor_cost
 print('You should buy/sell ' + str((new_pw_tkn - user_pwripor)) + ' more pwrIPOR')
 print('For a total of ' + str(new_pw_tkn) + ' pwrIPOR')
+print('Cost to purchase these pwrIPOR is ' + str(ipor_cost))
 print('You should buy/sell the following amounts of ipTokens')
 print(user_iptkns_chng)
 print('For a total amout of ipTkns')
 print(user_iptkns_df)
+print('Cost to purchase these ipTokens is ' + str(iptokn_cost))
 print('You should allocate your pwrIPOR tokens using this delegation strategy:')
 print(user_delegation)
 print('This would give you the following expected aprs in USD')
 print(user_aprs)
 print('With the total apr in USD across all staking pools of:')
 print(user_tot_aprs)
-print('This would cost a total of ' + str(cost))
+print('This would cost a total of ' + str(total_cost))
 print('This optimization took ' + str(time.time() - start_time) + ' seconds to run')
 
